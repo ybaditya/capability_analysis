adbe05d229b6ce0e55064e8a860e2bf472966a83
YB Aditya Tri Agam P <95304705+ybaditya@users.noreply.github.com>
2024-09-21 12:23:43 +0700

    Update i_cpcpk.py

--- i_cpcpk.py
+++ i_cpcpk.py
@@ -42,28 +42,64 @@ if uploaded_file:
     st.write("Select Columns to Analysis:")
     data_columns = st.multiselect("Columns", list(df.columns))
 
-    usl = st.number_input('Input USL :')
-    lsl = st.number_input('Input LSL :')
 
-    a_lsl = lsl - 3.0
-    a_usl = usl + 3.0
+st.sidebar.link_button("Link to Multi Apps & Tools", "https://multiappsandtools.web.app/")
+st.sidebar.markdown("---")
 
+st.sidebar.header('Input Capability Parameters :')
 
+cp_ok = st.sidebar.number_input('Input Cp Standard:',value=1.33)
+cpk_ok = st.sidebar.number_input('Input Cpk Standard:',value=1.33)
+
+st.sidebar.markdown("---")
+
+usl = st.sidebar.number_input('Input USL :')
+lsl = st.sidebar.number_input('Input LSL :')
+
+st.sidebar.markdown("---")
+
+cs = (usl + lsl) / 2
+a_lsl = lsl - 3
+a_usl = usl + 3
+
+Analyze = st.sidebar.button('Analyze')
+
+if Analyze:
     for i in range(len(data_columns)):
-        st.write(data_columns[i])
-        chart_data = pd.DataFrame({ 'x' : np.arange(30), 
-                               'y' : df[data_columns[i]]})
-        c = alt.Chart(chart_data).mark_line(point=True).encode(
-        x='x',
-        y=alt.Y('y',scale=alt.Scale(domain=[a_lsl , a_usl]))
+        st.markdown('---')
+        st.write("Graph for",data_columns[i])
+        st.write("**LSL :**",lsl,"**| CS :**",cs,"**| USL :**",usl,)
+        chart_data = pd.DataFrame({ 'x' : np.arange(30),'y' : df[data_columns[i]], 'usl' : np.repeat(usl, 30), 'lsl' : np.repeat(lsl, 30)})
+
+        line_data = alt.Chart(chart_data).mark_line(color='green',point=True).encode(
+            x='x',
+            y=alt.Y('y',scale=alt.Scale(domain=[a_lsl , a_usl]))
         )
+        line_usl = alt.Chart(chart_data).mark_line(color='red',point=True).encode(
+            x='x',
+            y=alt.Y('usl',scale=alt.Scale(domain=[a_lsl , a_usl])),
+        )
+        line_lsl = alt.Chart(chart_data).mark_line(color='red',point=True).encode(
+            x='x',
+            y=alt.Y('lsl',scale=alt.Scale(domain=[a_lsl , a_usl])),
+        )
+        c = alt.layer(line_data, line_usl, line_lsl)
         st.altair_chart(c, theme='streamlit', use_container_width=True)
-  
 
-    for i in range(len(data_columns)):
+        st.markdown('---')
         x = df[data_columns[i]]
-        st.write("**Cp-Cpk**", data_columns[i])
-        st.write("**CP :**",Cp(x, usl, lsl))
-        st.write("**CPK :**",Cpk(x, usl, lsl))
+        st.write("**Capability from:**", data_columns[i])
+        st.write("**Cp Result :**",Cp(x, usl, lsl))
+        st.write("**Cpk Result :**",Cpk(x, usl, lsl))
+        if Cp(x, usl, lsl) > cp_ok :
+            st.write(":green[**Cp Status :** OK]")
+        else:
+            st.write(":red[**Cp Status :** NG]")
+        
+        if Cpk(x, usl, lsl) > cpk_ok :
+            st.write(":green[**Cpk Status :** OK]")
+        else:
+            st.write(":red[**Cpk Status :** NG]")
+        st.markdown('---')
         fig = sns.displot(x, kde=True)
         st.pyplot(fig)
